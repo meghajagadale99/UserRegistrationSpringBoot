ec0f646290faa634e7670cdd05766e179d26052f
Megha <meghajagadale99@gmail.com>
2022-09-09 00:39:30 +0530

    Created User Registration form

---
+++ src/main/java/com/bridgelabz/UserRegistration/UserRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.UserRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration App");
+		SpringApplication.run(UserRegistrationApplication.class, args);
+	}
+
+}
